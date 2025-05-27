bdf9c5630efbc3823fded8bfe1e27bec51c982d1
Patrick Grüner <patrick.gruener@mercedes-benz.com>
2025-05-27 14:11:39 +0200

    feat: initial setup

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
+# The Blackjack module provides methods for parsing card values and making decisions in a game of Blackjack.
 module Blackjack
+  CARD_VALUES = {
+    'ace' => 11,
+    'eight' => 8,
+    'two' => 2,
+    'nine' => 9,
+    'three' => 3,
+    'ten' => 10,
+    'four' => 4,
+    'jack' => 10,
+    'five' => 5,
+    'queen' => 10,
+    'six' => 6,
+    'king' => 10,
+    'seven' => 7
+  }.freeze
+
   def self.parse_card(card)
-    value = {
-      'ace' => 11,
-      'eight' => 8,
-      'two' => 2,
-      'nine' => 9,
-      'three' => 3,
-      'ten' => 10,
-      'four' => 4,
-      'jack' => 10,
-      'five' => 5,
-      'queen' => 10,
-      'six' => 6,
-      'king' => 10,
-      'seven' => 7
-    }
-    value.default = 0
-    value[card]
+    CARD_VALUES.fetch(card, 0)
   end
 
   def self.card_range(card1, card2)
